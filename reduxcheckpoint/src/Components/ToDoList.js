c6b73a6b0e9f9bb07a68eddb09e593d30537d9a2
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-07-05 14:38:04 -0400

    Create ToDoList.js

---
+++ reduxcheckpoint/src/Components/ToDoList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import TaskCard from './TaskCard'
+
+function filterTasks(todos, filter) {
+
+  switch (filter) {
+    case "done":
+      return todos.filter((todo) => todo.isDone === true);
+    case "undone":
+      return todos.filter((todo) => todo.isDone === false);
+    case "all":
+    default:
+      return todos;
+  }
+}
+const ToDoList =({todos,filter})=>{
+  const result = filterTasks(todos, filter);
+    return (
+    <div className='list'>{result.map(el=><TaskCard key ={el.id} task ={el}/>)}</div>
+    );
+    
+}
+const mapStatetoProps =(state)=>{
+    return {
+      todos: state.tasks ,
+      filter: state.Filtered
+    }}
+export default connect(mapStatetoProps)(ToDoList)
