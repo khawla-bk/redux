a08a1ce1aa8ced6c08f732c1beabe34802d33899
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-07-06 08:36:45 -0400

    Update TodoList.js

--- reduxcheckpoint/src/containers/TodoList.js
+++ reduxcheckpoint/src/containers/TodoList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Todo from "../components/Todo";
 import { connect } from "react-redux";
-import { toggleTodo } from "../actions/todo.actions";
+import { toggleTodo } from "../Actions/todoActions";
 
 const TodoList = ({ todos, toggleTodo }) => {
   return (
