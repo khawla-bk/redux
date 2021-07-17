174c23ef248517f83aab8d69be59fda2e94460ff
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-07-17 10:43:47 -0400

    Update index.js

--- reduxcheckpoint/src/index.js
+++ reduxcheckpoint/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux'
-import Store from './Store'
+import store from './store'
 
 ReactDOM.render(
   <Provider store={Store}>
