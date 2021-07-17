23b6f5ecc4a071f66dfe859b0ae54a35319394ae
Khawla Ben Kadida <33607678+khawla-bk@users.noreply.github.com>
2021-07-17 10:38:06 -0400

    Delete reduxcheckpoint/src/reducers directory

--- reduxcheckpoint/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import todos from "./todo.reducers";
-import filterTodo from "./filter.reducers";
-
-export default combineReducers({
-  todos,
-  filterTodo
-});
